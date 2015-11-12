eb596f90d20dc1f3d6777602473c094560581e25
Andrew Weir <andru.weir@gmail.com>
2015-11-12 14:41:55 +0700

    Updated tests to pass

--- test/functional.js
+++ test/functional.js
@@ -18,18 +18,18 @@ describe('Gateway', function () {
     var s = new Server(host, 'test', api);
     s.start();
 
-    var r = new Request(host);
+    var r = new Request(host, 'test');
 
-    r.call('test', 'sum', [1, 7], function (err, response) {
+    r.call('sum', [1, 7], function (err, response) {
       response.should.equal(8);
     });
 
-    r.call('test', 'addFive', 5, function (err, response) {
+    r.call('addFive', 5, function (err, response) {
       console.log("RESPONSE", response);
       response.should.equal(10);
     });
 
-    r.call('test', 'nothinghere', {path: '/some/file/path.jpg'}, function(err, response) {
+    r.call('nothinghere', {path: '/some/file/path.jpg'}, function(err, response) {
       err.message.should.equal('Method test.nothinghere does not exist');
     });
 
